ee245e7880c106fe7e40ebe8fdf58aa7264f1c33
Didans <dawid.ns@hotmail.com>
2018-04-25 19:14:33 +0200

    Simple Main

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,63 @@
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Map;
 
 public class Main {
+    private static String script = "MiniSat+/run.sh";
+    private static String problem = "MiniSat+/Examples/garden9x9.opb";
+    private static String solution = "Solution/ans.txt";
 
     public static void main(String[] args) throws IOException {
-        System.out.println("Just run script and check if it works");
+        Process procBuildScript = new ProcessBuilder(script, problem).start();
+        System.out.println("Script is running...");
+        try {
+            procBuildScript.waitFor();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-        Process procBuildScript = new ProcessBuilder("MiniSat+/run.sh", "MiniSat+/Examples/garden9x9.opb").start();
+        BufferedReader br = null;
+        FileReader fr = null;
 
-        System.out.println("Yeah! It works");
+        try {
+
+            fr = new FileReader(solution);
+            br = new BufferedReader(fr);
+
+            String currentLine;
+            String previousLine = null;
+
+            while ((currentLine = br.readLine()) != null) {
+                if(currentLine.startsWith("s")) {
+                    System.out.println(previousLine);
+                    System.out.println(currentLine);
+                }
+                else if(currentLine.startsWith("v")){
+                    System.out.println(currentLine);
+                }
+                previousLine=currentLine;
+            }
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        } finally {
+
+            try {
+
+                if (br != null)
+                    br.close();
+
+                if (fr != null)
+                    fr.close();
+
+            } catch (IOException ex) {
+
+                ex.printStackTrace();
+
+            }
+
+        }
     }
 }
