75f3be7ee1b7a9e4e2b46a95c1122763c89d714d
Didans <dawid.ns@hotmail.com>
2018-04-25 18:43:09 +0200

    Run script

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        System.out.println("Just run script and check if it works");
+
+        Process procBuildScript = new ProcessBuilder("MiniSat+/run.sh", "MiniSat+/Examples/garden9x9.opb").start();
+
+        System.out.println("Yeah! It works");
     }
 }
