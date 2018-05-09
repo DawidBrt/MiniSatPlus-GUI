800d81b64f06d581ba362e767bc6b4b3028f5924
Didans <dawid.ns@hotmail.com>
2018-05-09 15:22:42 +0200

    Cleanup

--- MiniSat+/run.sh
+++ MiniSat+/run.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-echo "This script is about to run another script."
-ls
+# "This script is about to run another script."
 sh ./MiniSat+/minisat+_script $1 > Solution/ans.txt
-echo "This script has just run another script."
